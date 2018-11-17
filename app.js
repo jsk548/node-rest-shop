66e0557d4adb36227cf7cc91a4b07a4bbb243494
vinodhg <vinodhg@gendevs.com>
2018-11-17 23:55:11 +0530

    added CORS

--- app.js
+++ app.js
@@ -10,9 +10,20 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.use((req, res, next)=>{
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-requested-with, Content-Type, Accept, Authorization');
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+          return res.status(200).json({});
+    }
+    next();
+});
+
 //Routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
 
 app.use((req, res, next)=>{
     const error = new Error('Not Found');
